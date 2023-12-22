b2a56adea9608330a8d7a159209f6a5feb990983
Nandini Handa <75879947+nandinihanda@users.noreply.github.com>
2023-12-22 06:17:10 +0000

    arrays part one done

---
+++ 01_Basics/10_arrays.js
@@ -0,0 +1,27 @@
+const myarr=[0,1,2,3,4,5] //array with numbers
+
+const myarr1=["apple","mango","grapes"] //string array
+
+const myarr2=new Array(1,2,3,4) //another way of declaring an array
+console.log(myarr[1]) //fetching the element of array through index number
+
+//Array methods
+
+myarr.push(6)//add elements at the end of array
+console.log(myarr);
+
+myarr.pop() //remove the last elememnt of the array
+
+myarr.unshift(12)//add elements at the start of array
+myarr.shift() //remove the first elememnt of the array
+
+console.log(myarr.includes(9));
+console.log(myarr.indexOf(3));
+
+//join method joins anything with the array and also converts the array into string
+
+//slice and splice methods
+
+//slice method does not manipulates the original array while splice method manipulates the original array.
+
+
