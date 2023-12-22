ac06ce99cbd322d544e4c32f16be88c3cf5fd5c0
Nandini Handa <75879947+nandinihanda@users.noreply.github.com>
2023-12-22 05:19:40 +0000

    Numbers done

---
+++ 01_Basics/08_number.js
@@ -0,0 +1,20 @@
+const score=100 // one method to declare
+
+const number=new Number(200) // another method to declare
+console.log(number);
+
+console.log(number.toString());
+console.log(number.toString().length);
+console.log(number.toFixed(2));
+
+const newnumber=23.856
+console.log(newnumber.toPrecision(3));
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString("en-IN"));
+
+
+console.log(Math.abs(-5))
+console.log(Math.round(5.6));
+console.log(Math.ceil(5.2));
