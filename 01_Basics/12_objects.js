8f0f557f84989b598131b95783211b201853735d
Nandini Handa <75879947+nandinihanda@users.noreply.github.com>
2023-12-22 15:14:58 +0000

    objects done

---
+++ 01_Basics/12_objects.js
@@ -0,0 +1,16 @@
+//singleton
+//Object.create
+
+//object literals
+
+const jsuser={
+    name:"Nandini",
+    age:"25",
+    location:"Bangalore",
+    email:"nandini@gmail.com",
+    isLoggedIn:false,
+    lastlogin:["monday","friday"]
+}
+
+console.log(jsuser.email)
+console.log(jsuser["email"]);
