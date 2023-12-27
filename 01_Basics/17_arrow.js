dbc08aaf1be104b63153c01a1a26c3650a2ed5f0
Nandini Handa <75879947+nandinihanda@users.noreply.github.com>
2023-12-27 04:19:09 +0000

    arrow function done

--- 01_Basics/17_arrow.js
+++ 01_Basics/17_arrow.js
@@ -21,3 +21,29 @@ const user = {
 // }
 
 // coding()
+
+const coding =  () => {
+    let username = "nandini"
+    console.log(this);
+}
+
+
+// coding()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "nandini"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
